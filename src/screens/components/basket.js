2f69f11ded810947cfa2350c9a60cd923b088583
itu-itis21-karaz20 <karaz20@itu.edu.tr>
2022-12-01 17:46:09 +0300

    basket button updated

--- src/screens/components/basket.js
+++ src/screens/components/basket.js
@@ -2,12 +2,17 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getDataRequest } from '../../store/data/dataAction'
 import '../../styles/Main.css'
 import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom';
+import {secondary} from '../../constants/theme'
 
 function Basket({inBasket, setInBasket, ...props}){   
 
-    const products = useSelector((state) => state.data.products);
+    const a = useSelector((state) => state.data.products);
+    const b = useSelector((state=>state.categorizedProduct.categorizedProducts))
+    const products = a.length>0 ? a:b;
 
     function getOccurrence(array, value) {
         var count = 0;
@@ -19,6 +24,17 @@ function Basket({inBasket, setInBasket, ...props}){
         setInBasket(inBasket.filter(function(item) {
             return item !== id
         }))
+    }
+
+    const addProduct=(id)=>{
+        setInBasket(oldArray => [...oldArray, id])
+    }
+
+    const removeProduct=(id)=>{
+        let index = inBasket.indexOf(id);
+        if (index > -1) {
+            setInBasket(inBasket.splice(index, 1));
+        }       
     }
 
     return(
@@ -33,10 +49,33 @@ function Basket({inBasket, setInBasket, ...props}){
                     <div>{"Pieces: " + getOccurrence(inBasket, product.id)}</div>
                     <div>{"Price: " + product.price*getOccurrence(inBasket, product.id)}</div>
                 </div>
+
+                <div className='basketButton'>
+                    <div className='button'>
+                        <p>Delete</p>
+                        <IconButton  color="primary" aria-label="delete" onClick={()=>deleteProduct(product.id)}>
+                          <DeleteTwoToneIcon />                          
+                        </IconButton>
+                        
+                    </div>
+
+                    <div className='button'>
+                        <p>Add One More</p>
+                        <IconButton color="primary" aria-label="delete" onClick={()=>addProduct(product.id)}>
+                            <AddCircleOutlineIcon />                            
+                        </IconButton> 
+                        
+                    </div> 
+                    
+                    <div className='button'>
+                        <p>Remove One</p>
+                        <IconButton color="primary" aria-label="delete" onClick={()=>removeProduct(product.id)}>
+                          <RemoveCircleOutlineIcon />                          
+                        </IconButton>                        
+                    </div>
+                </div>
                 
-                <IconButton color="primary" aria-label="delete" onClick={()=>deleteProduct(product.id)}>
-                  <DeleteTwoToneIcon />
-                </IconButton> 
+                 
             </div> 
 
         ))}
